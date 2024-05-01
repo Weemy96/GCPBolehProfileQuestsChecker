d9f625a6c9b4ee50d39551880b5f2e967425c7a3
Weemy96 <Weemy96@users.noreply.github.com>
2024-05-01 21:36:11 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# GCPBoleh Profile Quests Checker
+
+This script is help for check and count badges form your Google Cloud Boosts Skill Profile.
+
+### GCPBoleh Season 6
+- April 25 - May 24, 2024
+
+**Requirements:**
+
+- Python 3.9 >=
+
+**Installation:**
+
+1. Open a terminal window.
+2. Navigate to the directory where your script is located.
+3. Install the required dependencies using the following command:
+
+  ```bash
+  python -m venv .venv
+  ./.venv/Scripts/activate
+  pip install -r requirements.txt
+  ```
+**Usage:**
+1. Open a terminal window.
+2. Navigate to the directory where your script is located.
+3. Run the script with the profile URL as an argument:
+4. 
+  ```bash
+  python skill.py [profile_URL]
+  ```
---
+++ skill.py
@@ -0,0 +1,106 @@
+from bs4 import BeautifulSoup
+import requests
+from tabulate import tabulate
+from datetime import datetime, timedelta
+import re
+import sys
+
+if len(sys.argv) < 2:
+    boostskillProfile_URL = input('Input your Google Cloud Skills Boost Profile URL: ')
+else:
+    boostskillProfile_URL = sys.argv[1]
+
+# Define the date range
+start_date_str = "Apr 25, 2024"
+end_date_str   = "May 24, 2024"
+
+
+# Define the time zone abbreviation and its corresponding offset
+timezone_abbrev = "EDT"
+timezone_offset = timedelta(hours=-4)  # EDT is UTC-4
+
+
+# Parse the date range
+start_date = datetime.strptime(start_date_str, "%b %d, %Y") + timezone_offset
+end_date = datetime.strptime(end_date_str, "%b %d, %Y") + timezone_offset
+
+webpage = requests.get(boostskillProfile_URL, headers={'User-Agent': 'Mozilla/5.0'})
+soup = BeautifulSoup(webpage.content, 'html.parser')
+
+quests_requested=[
+##AI/ML:
+#Skill Badges
+"Get Started with TensorFlow on Google Cloud",
+"Build LookML Objects in Looker",
+"Detect Manufacturing Defects using Visual Inspection AI",
+"Analyze Speech and Language with Google APIs",
+"Analyze Images with the Cloud Vision API",
+"Analyze Sentiment with Natural Language API",
+"Predict Soccer Match Outcomes with BigQuery ML",
+"Create ML Models with BigQuery ML",
+ 
+#Regular Badges
+"Integrating Applications with Gemini 1.0 Pro on Google Cloud",
+"Intro to ML: Language Processing",
+"Intro to ML: Image Processing",
+"Generative AI Explorer - Vertex AI",
+"Introduction to AI and Machine Learning on Google Cloud",
+"Google Cloud Big Data and Machine Learning Fundamentals",
+"Applying Machine Learning to your Data with Google Cloud",
+"Production Machine Learning Systems",
+"Smart Analytics, Machine Learning, and AI on Google Cloud",
+"ML Pipelines on Google Cloud",
+"Gemini for Data Scientists and Analysts",
+
+##Infrastructure & Security:
+#Skill Badges
+"Create and Manage AlloyDB Databases",
+"Manage PostgreSQL Databases on Cloud SQL",
+"Monitor and Manage Google Cloud Resources",
+"Manage Kubernetes in Google Cloud",
+"Build Infrastructure with Terraform on Google Cloud",
+
+#Regular Badges
+"Baseline: Infrastructure",
+"Google Cloud Computing Foundations: Infrastructure in Google Cloud - Locales",
+"Security Best Practices in Google Cloud",
+"Securing your Network with Cloud Armor",
+"Google Cloud Computing Foundations: Networking & Security in Google Cloud",
+"Mitigating Security Vulnerabilities on Google Cloud"
+]
+
+skill = []
+date_comp = []
+total_match = 0
+#### corse name
+for span in soup.findAll('span', {'class':'ql-title-medium l-mts'}):
+    quests_requested = span.get_text().replace('\n','')
+    # print(quests_requested)
+    skill.append(quests_requested)
+### corse complete date
+for span in soup.findAll('span', {'class':'ql-body-medium l-mbs'}):
+    date_comp.append(span.get_text().replace('\n',''))
+result = zip(skill,date_comp)
+
+
+# Filter and only show entries between the start and end dates
+filtered_data = []
+for entry in result:
+    date_match = re.search(r"(\w{3} \d{1,2}, \d{4})", entry[1])
+    if date_match:
+        date_str = date_match.group(1)
+        date_with_timezone = datetime.strptime(date_str, "%b %d, %Y") + timezone_offset
+        if start_date <= date_with_timezone <= end_date:
+            filtered_data.append(entry)
+
+print(tabulate(filtered_data,headers=["Quest Completed","Date of Complete"]))
+print("\nTotal: %d\n\n"%len(filtered_data))
+
+#Filter only match of quests_requested
+matched_entries = [entry for entry in filtered_data if any(skill in entry[0] for skill in quests_requested)]
+
+
+print(tabulate(matched_entries,headers=["Quest Metched","Date of Complete"]))
+print("\nTotal: "+ str(len(matched_entries)))
+if(len(matched_entries)<= 0):
+    print("Maybe your Profile URL wrong / Profile URL not public and accessible / Not earned the badge")
